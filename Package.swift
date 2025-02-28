cf9b4cdd3d59988b2af4968991e6780613759914
curdicu <curdicu@gmail.com>
2025-02-28 21:43:23 +0800

    *

--- Package.swift
+++ Package.swift
@@ -14,16 +14,16 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/curdicu/BitcoinCore.git", .branch("main")),
-        .package(url: "https://github.com/curdicu/BitcoinCashKit.git", .branch("main")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCore.Swift.git", .upToNextMajor(from: "3.0.0")),
+        .package(url: "https://github.com/horizontalsystems/BitcoinCashKit.Swift.git", .upToNextMajor(from: "3.0.0")),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
     ],
     targets: [
         .target(
             name: "ECashKit",
             dependencies: [
-                .product(name: "BitcoinCore", package: "BitcoinCore"),
-                .product(name: "BitcoinCashKit", package: "BitcoinCashKit"),
+                .product(name: "BitcoinCore", package: "BitcoinCore.Swift"),
+                .product(name: "BitcoinCashKit", package: "BitcoinCashKit.Swift"),
                 .product(name: "SwiftProtobuf", package: "swift-protobuf"),
             ]
         ),
